4b4a209fe8c11a1768bff4bab9da58030838ea7c
andrenevares <andre.nevares@gmail.com>
2020-08-16 22:11:22 -0300

    quase-finalizada-home

--- project/blog/views.py
+++ project/blog/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage(request):
-    return render (request, template_name="blog/home.html")
+    title = "Home"
+    return render (request, 'blog/home.html', {'title': title})
+
+
